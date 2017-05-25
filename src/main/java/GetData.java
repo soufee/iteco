1891f8a08a2ca0810a1e50b1d40b6594f79a386a
Shomakhov <soufee@mail.ru>
2017-05-25 19:45:54 +0300

    with logger

--- src/main/java/GetData.java
+++ src/main/java/GetData.java
@@ -1,3 +1,5 @@
+import org.apache.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -10,55 +12,65 @@ import java.util.*;
  * Created by admin on 25.05.2017.
  */
 public class GetData {
-    public static Map<String, String> entries = new HashMap<String, String>();
-    public static Map<String, String> exits = new HashMap<String, String>();
-    public static Set<String> set = new HashSet<String>();
-    public static String line = "";
-    // public String fileName;
-    // public List<MethodCortege> list = new ArrayList<MethodCortege>();
 
-//    public GetData(String fileName) {
-//        this.fileName = fileName;
-//    }
+    private static Map<String, String> entries = new HashMap<String, String>();
+    private static Map<String, String> exits = new HashMap<String, String>();
+    private static Set<String> set = new HashSet<String>();
+    private static String line = "";
+    private static final Logger logger = Logger.getLogger(GetData.class);
 
-    public void soutResults(String fileName) throws ParseException {
+    public void soutResults(String fileName)  {
+        logger.debug("Starting...");
         fillInMaps(fileName);
-        ArrayList<MethodCortege> list = (ArrayList<MethodCortege>) getList();
+        ArrayList<MethodCortege> list = null;
+        try {
+            list = (ArrayList<MethodCortege>) getList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+        }
         for (int i = 0; i < list.size(); i++) {
             set.add(list.get(i).getMethod());
         }
 
         for (String string : set) {
-            System.out.println("OperationsImpl:" + string + " min " + getMinTime(string, list) + " max " + getMaxTime(string, list)
+            System.out.println(string + " min " + getMinTime(string, list) + " max " + getMaxTime(string, list)
                     + " avg " + getAvgTime(string, list) + " max id " + getMaxId(string, list) + " count " + count(string, list));
         }
-
+logger.debug("Started...");
     }
 
 
-    public void fillInMaps(String fileName) {
+    private void fillInMaps(String fileName) {
         try {
+            logger.debug("Trying to fill maps");
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
             while ((line = reader.readLine()) != null) {
                 shareToMap(line);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            logger.error(e.getMessage());
         } catch (IOException e) {
             e.printStackTrace();
+            logger.error(e.getMessage());
         }
-        System.out.println("В entries " + entries.size() + " записей");
-        System.out.println("В exits " + exits.size() + " записей");
-
     }
 
-    //  public List<MethodCortege> list = new ArrayList<MethodCortege>();
 
-    public List<MethodCortege> getList() throws ParseException {
+
+    private List<MethodCortege> getList()  {
+        logger.debug("getting list");
         List<MethodCortege> result = new ArrayList<MethodCortege>();
         for (Map.Entry<String, String> entry : entries.entrySet()) {
             String string = entry.getValue();
-            long time = getTime(getMethodNameWithId(string));
+            long time = 0;
+            try {
+                time = getTime(getMethodNameWithId(string));
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+                e.printStackTrace();
+            }
             if (time != -1) {
                 MethodCortege cortege = new MethodCortege(getMethodName(entry.getKey()), getMethodId(entry.getKey()), (int) time);
                 result.add(cortege);
@@ -67,7 +79,7 @@ public class GetData {
         return result;
     }
 
-    public void shareToMap(String string) {
+    private void shareToMap(String string) {
 
         if (string.contains("entry")) {
             entries.put(getMethodNameWithId(string), string);
@@ -76,7 +88,7 @@ public class GetData {
         }
     }
 
-    public long getTime(String string) throws ParseException {
+    private long getTime(String string)  {
         if (exits.containsKey(string)) {
             String entry = entries.get(string);
             String exit = exits.get(string);
@@ -86,41 +98,41 @@ public class GetData {
         } else return -1;
     }
 
-    public long getDate(String string) throws ParseException {
-        String year = string.substring(0, 4);
-        String month = string.substring(5, 7);
-        String day = string.substring(8, 10);
-        String hours = string.substring(11, 13);
-        String minutes = string.substring(14, 16);
-        String seconds = string.substring(17, 19);
-        String milliseconds = string.substring(20, 23);
+    private long getDate(String string) {
+
         String d = string.substring(0, 23);
-        Calendar calendar = new GregorianCalendar(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day), Integer.parseInt(hours), Integer.parseInt(minutes), Integer.parseInt(seconds));
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss,SSS");
-        Date date = simpleDateFormat.parse(d);
+      SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss,SSS");
+        Date date = null;
+        try {
+            date = simpleDateFormat.parse(d);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+        }
         long time = date.getTime();
 
         return time;
     }
 
-    public String getMethodNameWithId(String line) {
-        String methodWithId = line.substring(line.indexOf('(') + 1, line.indexOf(')'));
+    private String getMethodNameWithId(String line) {
+        String classname = line.substring(line.indexOf('[') + 1, line.indexOf(']'));
+        String methodWithId = classname + ":" + line.substring(line.indexOf('(') + 1, line.indexOf(')'));
         //  System.out.println(methodWithId);
         return methodWithId;
     }
 
-    public String getMethodName(String line) {
-        String methodName = line.substring(0, line.indexOf(":"));
+    private String getMethodName(String line) {
+        String methodName = line.substring(0, line.lastIndexOf(":"));
         return methodName;
     }
 
-    public int getMethodId(String line) {
+    private int getMethodId(String line) {
 
-        int methodId = Integer.parseInt(line.substring(line.indexOf(":") + 1));
+        int methodId = Integer.parseInt(line.substring(line.lastIndexOf(":") + 1));
         return methodId;
     }
 
-    public int getMaxId(String methodName, List<MethodCortege> list) {
+    private int getMaxId(String methodName, List<MethodCortege> list) {
         int maxId = 0;
         int maxTime = 0;
         for (MethodCortege cortege : list) {
@@ -132,7 +144,7 @@ public class GetData {
         return maxId;
     }
 
-    public int getMaxTime(String methodName, List<MethodCortege> list) {
+    private int getMaxTime(String methodName, List<MethodCortege> list) {
         int maxTime = 0;
 
         for (MethodCortege cortege : list) {
@@ -142,7 +154,7 @@ public class GetData {
     }
 
 
-    public int getMinTime(String methodName, List<MethodCortege> list) {
+    private int getMinTime(String methodName, List<MethodCortege> list) {
         int minTime = Integer.MAX_VALUE;
 
         for (MethodCortege cortege : list) {
@@ -154,7 +166,7 @@ public class GetData {
         return minTime;
     }
 
-    public long getAvgTime(String methodName, List<MethodCortege> list) {
+    private long getAvgTime(String methodName, List<MethodCortege> list) {
         int count = 0;
         long avg = 0;
         for (MethodCortege cortege : list) {
@@ -167,7 +179,7 @@ public class GetData {
         return avg / count;
     }
 
-    public int count(String methodName, List<MethodCortege> list) {
+    private int count(String methodName, List<MethodCortege> list) {
         int counter = 0;
         for (MethodCortege cortege : list) {
             if (cortege.getMethod().equals(methodName))
@@ -175,5 +187,6 @@ public class GetData {
         }
         return counter;
     }
+
 
 }
---
+++ src/main/java/MyCortege.java
@@ -0,0 +1,48 @@
+/**
+ * Created by admin on 25.05.2017.
+ *
+ *
+ *
+ */
+public class MyCortege<A, B, C, D, E, F> {
+    public A a;
+    public B b;
+    public C c;
+    public D d;
+    public E e;
+    public F f;
+
+
+    public MyCortege(A a, B b, C c, D d, E e, F f) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+    }
+
+    public A getA() {
+        return a;
+    }
+
+    public B getB() {
+        return b;
+    }
+
+    public C getC() {
+        return c;
+    }
+
+    public D getD() {
+        return d;
+    }
+
+    public E getE() {
+        return e;
+    }
+
+    public F getF() {
+        return f;
+    }
+}
